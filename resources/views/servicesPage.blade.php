286573493a4698df7b1492702dfcb0ccfdbe7243
alwierdi <alwi.erdi4@gmail.com>
2024-12-04 11:57:52 +0700

    feat: add footer section

--- resources/views/servicesPage.blade.php
+++ resources/views/servicesPage.blade.php
@@ -65,6 +65,16 @@
             text-align: center;
             margin-bottom: 20px;
         }
+
+        .footer-bg {
+            background: linear-gradient(to right, #5EB47C, #007D6E);
+            color: white;
+            padding: 80px 0;
+        }
+
+        .text-body-secondary {
+            color: white !important;
+        }
     </style>
     <title>Serices - HPV Vaccination</title>
 </head>
@@ -101,7 +111,7 @@
                 <div class="d-flex align-items-center">
                     <a class="btn contact-btn text-white me-3" href="#">Contact Us</a>
                     <a href="#" class="d-flex align-items-center">
-                        <i class="fas fa-user-circle fa-lg"></i>
+                        <i class="fas fa-user-circle fa-lg">Profile</i>
                     </a>
                 </div>
             </div>
@@ -149,6 +159,64 @@
             </div>
         </div>
     </section>
+
+    <footer class="p-5 footer-bg">
+        <div class="row">
+            <div class="col-6 col-md-2 mb-3">
+                <h5>Service</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Psychotherapy</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Mental Counselling</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Support Groups</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Case Management</a></li>
+                </ul>
+            </div>
+
+            <div class="col-6 col-md-2 mb-3">
+                <h5>Contact</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">+62 NOMOR FANES</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">MUSAWA@gmail.com</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">SOKIN POSISI</a></li>
+                </ul>
+            </div>
+
+            <div class="col-6 col-md-2 mb-3">
+                <h5>Links</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Privacy Policy</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Terms Of Use</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-5 offset-md-1 mb-3">
+                <form>
+                    <h5>Subscribe to our newsletter</h5>
+                    <p>Monthly digest of what's new and exciting from us.</p>
+                    <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                        <label for="newsletter1" class="visually-hidden">Email address</label>
+                        <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                        <button class="btn btn-primary" type="button">Subscribe</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="d-flex flex-column flex-sm-row justify-content-center py-4 my-4 border-top">
+            <p>© 2024 Company, Inc. All rights reserved.</p>
+            <ul class="list-unstyled d-flex">
+                <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
+                            <use xlink:href="#twitter"></use>
+                        </svg></a></li>
+                <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
+                            <use xlink:href="#instagram"></use>
+                        </svg></a></li>
+                <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
+                            <use xlink:href="#facebook"></use>
+                        </svg></a></li>
+            </ul>
+        </div>
+    </footer>
 
     <script src="{{ asset('bootstrap/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js') }}" defer></script>
 </body>

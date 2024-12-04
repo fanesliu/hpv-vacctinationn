81f5ffd4becd7de6615a2c7bbecc26c7c9eefd98
Winsen <winsen.wijaya002@binus.ac.id>
2024-12-04 10:06:48 +0700

    backUp local Winsen

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -6,9 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Profile</title>
     <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
 </head>
 <body>
     
-    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>

2260f76ac39a441e173a514b25e38db735019329
Fanes Liu <fns.liu1@gmail.com>
2025-01-09 16:05:06 +0700

    chore: add meta content security

--- resources/views/components/userLayout.blade.php
+++ resources/views/components/userLayout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link rel="stylesheet" href="{{ asset('bootstrap/bootstrap-5.3.3-dist/css/bootstrap.min.css') }}">
     <style>
         .primaryBos {
